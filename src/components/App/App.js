2ad4b04d19b3b2c5c38e3f8a17677d4fe86b5f3b
Etienne Tremel <etremel@travix.com>
2015-09-04 12:05:17 +0200

    Fix eslint

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 class App extends React.Component {
+  displayName: 'App'
+
   render() {
     return (
       <div className="app">
--- test/components/App/app.spec.js
+++ test/components/App/app.spec.js
@@ -1,6 +1,3 @@
-import chai from 'chai';
-import App from '../../../src/components/App/App.js';
-
 describe('App', () => {
   it('should do something', () => {
 
